1d5fbbf03092baefe04072ebd3bcaaee83e3e2b6
adriaanvermeire <adriaan.vermeire@student.kdg.be>
2018-01-11 18:44:29 +0100

    Begin testing with template matching in OpenCV

---
+++ imagematch0.py
@@ -0,0 +1,25 @@
+import cv2
+if __name__ == '__main__':
+    # TODO: get images from commandline
+    source = cv2.imread('image.jpeg')
+    pattern = cv2.imread('black.JPG')
+    match = cv2.imread('image.jpeg')
+
+    # img.shape returns tuple (height, width, # color channels (usually 3))
+    # [:2] limits the tuple returned to the first 2 (ignoring the color channels)
+    patternHeight, patternWidth = pattern.shape[:2]
+
+    result = cv2.matchTemplate(source, pattern, cv2.TM_SQDIFF)
+    # After the function finishes the comparison,
+    # the best matches can be found as global minimums (when CV_TM_SQDIFF was used)
+    # or maximums (when CV_TM_CCORR or CV_TM_CCOEFF was used)
+    # using the minMaxLoc() function
+    minLoc, maxLoc = cv2.minMaxLoc(result)[2:]
+
+    # to draw a rectangle at matching spot we need the upperleft corner (the returned point from minMaxLoc)
+    # and lowerright corner (calculated with pattern's width and height)
+    upperLeft = minLoc
+    lowerRight = (minLoc[0] + patternWidth, minLoc[1] + patternHeight)
+    cv2.rectangle(match, upperLeft, lowerRight, (255,0,0), -1)
+
+    cv2.imwrite('match.jpeg', match)
